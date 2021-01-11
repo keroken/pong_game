43f96ec15a5633f7d4b912f3232c5cbd56c0dae4
keroken <kenichi.shinagawa@gmail.com>
2021-01-11 22:37:23 +0900

    move paddles

--- main.lua
+++ main.lua
@@ -6,9 +6,12 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   smallFont = love.graphics.newFont('font.ttf', 8)
+  scoreFont = love.graphics.newFont('font.ttf', 32)
   love.graphics.setFont(smallFont)
 
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -16,6 +19,28 @@ function love.load()
     resizable = false,
     vsync = true
   })
+
+  player1Score = 0
+  player2Score = 0
+
+  player1Y = 30
+  player2Y = VIRTUAL_HEIGHT - 50
+end
+
+function love.update(dt)
+  -- player1 movement
+  if love.keyboard.isDown('w') then
+    player1Y = player1Y + -PADDLE_SPEED * dt
+  elseif love.keyboard.isDown('s') then
+    player1Y = player1Y + PADDLE_SPEED * dt
+  end
+
+  -- player2 movement
+  if love.keyboard.isDown('up') then
+    player2Y = player2Y + -PADDLE_SPEED * dt
+  elseif love.keyboard.isDown('down') then
+    player2Y = player2Y + PADDLE_SPEED * dt
+  end
 end
 
 function love.keypressed(key)
@@ -34,10 +59,10 @@ function love.draw()
   love.graphics.printf('Hello Pong', 0, 20, VIRTUAL_WIDTH, 'center')
 
   -- fist paddle(left)
-  love.graphics.rectangle('fill', 10, 30, 5, 20)
+  love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
   -- second paddle(right)
-  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
   -- ball(center)
   love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
